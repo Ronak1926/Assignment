7ed20dbe97a08440ccdb2c22337918e584da46b0
Ronak <ronaksojitra1926@gmail.com>
2025-12-30 15:14:44 +0530

    change the backend url

--- DSA_sheet_Frontend/src/utils/api.ts
+++ DSA_sheet_Frontend/src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://ec2-54-226-139-94.compute-1.amazonaws.com:5000",
+  baseURL: "https://assigment.duckdns.org",
   withCredentials: true,
 });
 
