6e15f352a21556a9514a44b6ce3d23e1770cef95
Ronak <ronaksojitra1926@gmail.com>
2025-12-30 19:17:31 +0530

    change the url

--- DSA_sheet_Frontend/src/utils/api.ts
+++ DSA_sheet_Frontend/src/utils/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Use env var if provided, otherwise default to deployed backend URL.
 // For local dev, set VITE_API_URL=http://localhost:5000
-const baseURL = "https://assignment.duckdns.org";
+const baseURL = "https://dsa-sheet-backend-9v6d.onrender.com"
 
 const api = axios.create({
   baseURL,
